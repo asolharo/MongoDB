641fdc1b8503d85b228fb2e52c7fc051a4d73e80
Angel Solano <solanoangel@icloud.com>
2022-06-04 20:32:10 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express()
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const { Timestamp } = require("bson");
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect("mongodb+srv://asolharo:UDvmMlRCyaaC0lft@cluster0.9nll8ek.mongodb.net/appointments")
+
+//create data schema
+const apptSchema = {
+    interviewer: String,
+    date: Date,
+    time: Timestamp,
+    name: String,
+    reason: String,
+}
+
+const Appt = mongoose.model("Appointment", apptSchema)
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+
+app.post("/", function(req, res) {
+    let newAppt = new Appt({
+        interviewer: req.body.interviewer,
+        date: req.body.date,
+        time: req.body.time,
+        name: req.body.name,
+        reason: req.body.reason,
+    })
+    newAppt.save()
+    res.redirect('/')
+})
+
+app.listen(1987, function() {
+    console.log("server connected and running")
+})
